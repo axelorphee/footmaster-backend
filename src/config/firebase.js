0c61aa6ec58504a99fd159cbb0fa0ef62b496c6d
Axel <axelorphee@gmail.com>
2026-03-11 16:48:58 +0300

    FANTASYFOOTBALLMILIEU

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,23 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
 
+let app;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
+    app = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } else {
+    const serviceAccount = require('./firebase-service-account.json');
+
+    app = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
+} else {
+  app = admin.app();
+}
 
 module.exports = admin;
