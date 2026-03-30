3a7cbc2fac85f386865caf662edb79529e3bdc20
Axel <axelorphee@gmail.com>
2026-03-31 00:08:05 +0300

    ADD NOTIFICATIONS

--- src/controllers/notification_engine.controller.js
+++ src/controllers/notification_engine.controller.js
@@ -12,3 +12,42 @@ exports.runNotificationEngine = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.startNotificationEngine = async (req, res, next) => {
+  try {
+    const data = await notificationEngineService.startNotificationEngine();
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.stopNotificationEngine = async (req, res, next) => {
+  try {
+    const data = await notificationEngineService.stopNotificationEngine();
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getNotificationEngineStatus = async (req, res, next) => {
+  try {
+    const data = await notificationEngineService.getNotificationEngineStatus();
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/notificationEngine.routes.js
+++ src/routes/notificationEngine.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const controller = require('../controllers/notification_engine.controller');
 
+router.get('/status', controller.getNotificationEngineStatus);
+router.post('/start', controller.startNotificationEngine);
+router.post('/stop', controller.stopNotificationEngine);
 router.post('/run', controller.runNotificationEngine);
 
 module.exports = router;
