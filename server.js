568d342cc9adacfe3ea1d38dfb19f18f332dc7f4
Axel <axelorphee@gmail.com>
2026-04-01 18:28:46 +0300

    ADD NOTIFICATIONS ALL

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ require('dotenv').config();
 require('./src/config/database');
 
 const app = require('./src/app');
+const notificationEngineControlService = require('./src/services/notificationEngineControl.service');
+const notificationEngineService = require('./src/services/notification_engine.service');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function boot() {
+  const server = app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  try {
+    const control =
+      await notificationEngineControlService.getNotificationEngineControl();
+
+    if (control.enabled === true) {
+      await notificationEngineService.startNotificationEngine();
+      console.log('Notification engine auto-started from global control');
+    } else {
+      console.log('Notification engine global control is disabled');
+    }
+  } catch (err) {
+    console.error('Notification engine boot check error:', err.message);
+  }
+
+  return server;
+}
+
+boot();
--- src/controllers/notificationEngineAdmin.controller.js
+++ src/controllers/notificationEngineAdmin.controller.js
@@ -1,4 +1,5 @@
 const controlService = require('../services/notificationEngineControl.service');
+const notificationEngineService = require('../services/notification_engine.service');
 
 exports.getNotificationEngineControl = async (req, res, next) => {
   try {
@@ -16,11 +17,15 @@ exports.getNotificationEngineControl = async (req, res, next) => {
 exports.enableNotificationEngineControl = async (req, res, next) => {
   try {
     const data = await controlService.setNotificationEngineControl(true);
+    const engine = await notificationEngineService.startNotificationEngine();
 
     res.json({
       success: true,
       message: 'Notification engine globally enabled',
-      data,
+      data: {
+        control: data,
+        engine,
+      },
     });
   } catch (err) {
     next(err);
@@ -30,11 +35,15 @@ exports.enableNotificationEngineControl = async (req, res, next) => {
 exports.disableNotificationEngineControl = async (req, res, next) => {
   try {
     const data = await controlService.setNotificationEngineControl(false);
+    const engine = await notificationEngineService.stopNotificationEngine();
 
     res.json({
       success: true,
       message: 'Notification engine globally disabled',
-      data,
+      data: {
+        control: data,
+        engine,
+      },
     });
   } catch (err) {
     next(err);
